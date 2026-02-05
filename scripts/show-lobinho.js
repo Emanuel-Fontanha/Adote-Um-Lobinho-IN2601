f3e87b2445a9baa648ba2d24fd24b50e88ef49a1
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-02-05 11:35:19 -0300

    feat: estruturação do js determinando as funções a serem utilizadas (show-lobinho.js)

--- scripts/show-lobinho.js
+++ scripts/show-lobinho.js
@@ -1,1 +1,17 @@
 //para carregar a página com informações do lobinho
+
+async function buscarLobo(id){
+    
+}
+
+function carregarHtml(){
+
+}
+
+async function carregarLobo(id){
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    carregarLobo();
+});
