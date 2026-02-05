b8610f419134c68d3b53b73629e6f3e3447662fd
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-02-04 23:18:14 -0300

    feat: adição das funções de sortear lobos, de atualizar o html utilizando DOM, de carregar na página e do eventListener para o conteúdo carregado ao inciar a página (home-page.css)

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -18,4 +18,48 @@ async function buscarLobinhos() {
     }
 }
 
-buscarLobinhos()
+function sortearDois(lobos) {
+    const indicePrimeiroLobo = Math.floor(Math.random() * lobos.length);
+    let indiceSegundoLobo = Math.floor(Math.random() * lobos.length);
+
+    while (indicePrimeiroLobo === indiceSegundoLobo) {
+        indiceSegundoLobo = Math.floor(Math.random() * lobos.length);
+    }
+
+    return [lobos[indicePrimeiroLobo], lobos[indiceSegundoLobo]];
+}
+
+function atualizarHTML(lobo1, lobo2) {
+
+    const box1 = document.querySelector(".img-name-infos-box-1");
+    box1.querySelector("h4").textContent = lobo1.nome;
+    box1.querySelector("h5").textContent = `Idade: ${lobo1.idade} anos`;
+    box1.querySelector("p").textContent = lobo1.descricao;
+    box1.querySelector(".wolf-img-1").style.backgroundImage = `url(${lobo1.imagem})`;
+
+
+    const box2 = document.querySelector(".img-name-infos-box-2");
+    box2.querySelector("h4").textContent = lobo2.nome;
+    box2.querySelector("h5").textContent = `Idade: ${lobo2.idade} anos`;
+    box2.querySelector("p").textContent = lobo2.descricao;
+    box2.querySelector(".wolf-img-2").style.backgroundImage = `url(${lobo2.imagem})`;
+}
+
+async function carregarDoisLobos() {
+    try {
+        const lobinhos = await buscarLobinhos();
+
+        if (lobinhos.length < 2) return;
+
+        const [lobo1, lobo2] = sortearDois(lobinhos);
+
+        atualizarHTML(lobo1, lobo2);
+
+    } catch (error) {
+        console.error("Erro ao carregar lobos:", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    carregarDoisLobos();
+});
