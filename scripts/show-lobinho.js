4fcadb65dae8d04519cec8dc8f1e47f7376baa50
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-02-06 18:28:47 -0300

    feat: adição ,na arrow function, para retornar a página ao clicar no botão de deletar; exclusão do arquivo deletar-lobinho.js e transferência de ser conteúdo para o arquivo show-lobinho.js (show-lobinho.js)

--- scripts/show-lobinho.js
+++ scripts/show-lobinho.js
@@ -52,4 +52,24 @@ document.addEventListener("DOMContentLoaded", () => {
     carregarLobo(id);
 });
 
+async function deletarLobinho(id) {
+    try {
+        const response = await fetch(`http://localhost:3000/lobinhos/${id}`, {
+            method: 'DELETE'
+        });
 
+        if (!response.ok) {
+            throw new Error(`Erro HTTP! Status ${response.status}`);
+        }
+
+        console.log(`Lobinho com id ${id} foi deletado com sucesso`);
+        return true;
+    } catch (error) {
+        console.error('Erro ao deletar lobinho:', error);
+        throw error;
+    }
+}
+
+let del_btn = document.querySelector("#del-btn");
+del_btn.addEventListener("click", ()=>{ deletarLobinho(`${id}`); window.history.back();
+});
