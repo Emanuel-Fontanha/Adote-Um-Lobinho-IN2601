db65b9f80a3890a387e5cedd4a819b44b0975911
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-06 21:05:00 -0300

    feat: função para buscar lobinhos pelo nome

--- scripts/api.js
+++ scripts/api.js
@@ -17,3 +17,23 @@ export async function buscarLobinhos() {
 
     }
 }
+
+export async function buscarLobinhos_porNome(nome) {
+    try {
+
+        const response = await fetch(`http://localhost:3000/lobinhos?nome_like=${nome}`);
+
+        if (!response.ok) {
+            throw new Error(`Erro HTTP! Status: ${response.status}`);
+        }
+
+        const lobinhos = await response.json()
+        return lobinhos
+
+    } catch(error) {
+
+        console.error("Erro ao buscar lobinhos:", error)
+        throw error
+
+    }
+}
