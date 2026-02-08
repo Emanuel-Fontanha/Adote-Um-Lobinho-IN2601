ef8f01a87c210297fc492ad37fe864d24679abe1
Emanuel Fontanha <emanuellira@id.uff.br>
2026-02-08 16:14:30 -0300

    feat: finalizando a paginação da list-lobinhos

--- scripts/list-lobinhos.js
+++ scripts/list-lobinhos.js
@@ -1,9 +1,7 @@
 import { handleLobinhos } from "./api.js"
 
 
-function atualizarLayout(dados) {
-
-    const { lobos, pagina, totalItens, totalPaginas } = dados
+function atualizarLayout(lobos) {
     
     const lobosSection = document.querySelector(".view-wolves-section")
     lobosSection.innerHTML = ""
@@ -41,18 +39,91 @@ function atualizarLayout(dados) {
 }
 
 
+async function mudarPagina(novaPagina) {
+    const nomeLobo = returnTextFromFilter()
+    const box = document.getElementById("adopt-checkbox")
+    const dados = await handleLobinhos({
+        nome: nomeLobo,
+        pagina: novaPagina,
+        adotado: box.checked
+    })
+
+    atualizarLayout(dados.lobos)
+    montarPaginacao(dados.pagina, dados.totalPaginas)
+}
+
+
+function criarPontinhosPaginacao (box) {
+
+    // Caso não apareça a primeira ou a última página entre as 5 visíveis, cria os "..."
+
+    const hiddenPages = document.createElement("span")
+    hiddenPages.textContent = "..."
+    hiddenPages.classList.add("page-btn", "inactive")
+    box.appendChild(hiddenPages)
+}
+
+
+function montarPaginacao(pagina, totalPaginas) {
+    const box = document.querySelector(".pagination-div")
+    box.innerHTML = ""
+
+    let start = Math.max(1, pagina - 2)
+    let end = start + 4
+
+    if (end > totalPaginas) {
+        end = totalPaginas
+        start = Math.max(1, end - 4)
+    }
+
+    // Criando o botão << inicial
+    const arrowBtnStart = document.createElement("button")
+    arrowBtnStart.textContent = "<<"
+    arrowBtnStart.onclick = () => mudarPagina(1)
+    arrowBtnStart.classList.add("page-btn")
+    box.appendChild(arrowBtnStart)
+
+    if (start > 1) criarPontinhosPaginacao(box)
+
+    // Definindo as páginas visíveis
+    for (let i = start; i <= end; i++) {
+        const pageBtn = document.createElement("button")
+        pageBtn.textContent = i
+        pageBtn.classList.add("page-btn")
+
+        if (i === pagina) {
+            pageBtn.classList.add("current-page")
+        }
+
+        pageBtn.onclick = () => {mudarPagina(i)}
+        box.appendChild(pageBtn)
+    }
+
+    if (end < totalPaginas) criarPontinhosPaginacao(box)
+
+    // Criando o botão >> final
+    const arrowBtnEnd = document.createElement("button")
+    arrowBtnEnd.textContent = ">>"
+    arrowBtnEnd.onclick = () => mudarPagina(totalPaginas)
+    arrowBtnEnd.classList.add("page-btn")
+    box.appendChild(arrowBtnEnd)
+}
+
+
 function returnTextFromFilter() {
     const name = document.getElementById("wolf-filter").value
     return name.length > 0 ? name : ""
 }
 
 
+
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const dados = await handleLobinhos();
-        atualizarLayout(dados);
+        const dados = await handleLobinhos()
+        atualizarLayout(dados.lobos)
+        montarPaginacao(dados.pagina, dados.totalPaginas)
     } catch (error) {
-        console.error("Erro ao carregar:", error);
+        console.error("Erro ao carregar:", error)
     }
 });
 
@@ -63,11 +134,12 @@ box.addEventListener("change", async () => {
     
         const nomeLobo = returnTextFromFilter()
         isFiltering = isFiltering ? false : true
-        const dados = await handleLobinhos({ nome : nomeLobo, adotado : box.checked });
-        atualizarLayout(dados);
+        const dados = await handleLobinhos({ nome : nomeLobo, adotado : box.checked })
+        atualizarLayout(dados.lobos)
+        montarPaginacao(dados.pagina, dados.totalPaginas)
 
     } catch (error) {
-        console.error("Erro ao carregar:", error);
+        console.error("Erro ao carregar:", error)
     }
 })
 
@@ -76,11 +148,12 @@ document.addEventListener("keydown", async (e) => {
         try {
             
             const nomeLobo = returnTextFromFilter()
-            const dados = await handleLobinhos({ nome : nomeLobo, adotado : box.checked });
-            atualizarLayout(dados);
+            const dados = await handleLobinhos({ nome : nomeLobo, adotado : box.checked })
+            atualizarLayout(dados.lobos)
+            montarPaginacao(dados.pagina, dados.totalPaginas)
 
         } catch (error) {
-            console.error("Erro ao carregar:", error);
+            console.error("Erro ao carregar:", error)
         }
     }
 })
