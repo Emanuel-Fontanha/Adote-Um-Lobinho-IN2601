172b54d3e05313c55c6bfc0d141b8c0e831dedc6
Emanuel Fontanha <emanuellira@id.uff.br>
2026-02-08 02:13:22 -0300

    fix: correção de funcionamento dos filtros de texto e checkbox

--- scripts/list-lobinhos.js
+++ scripts/list-lobinhos.js
@@ -1,15 +1,14 @@
-import { buscarLobinhos } from "./api.js"
+import { handleLobinhos } from "./api.js"
 
-async function carregarLobinhos() {
 
-}
+function atualizarLayout(dados) {
 
-function atualizarLayout(lobinhos) {
+    const { lobos, pagina, totalItens, totalPaginas } = dados
     
     const lobosSection = document.querySelector(".view-wolves-section")
     lobosSection.innerHTML = ""
 
-    lobinhos.forEach((lobinho, index) => {
+    lobos.forEach((lobinho, index) => {
         const div = document.createElement("div")
         div.innerHTML = `
             <div class="wolf-container${index % 2 == 0 ? "-1" : "-2"}">
@@ -20,8 +19,8 @@ function atualizarLayout(lobinhos) {
                     class="wolf-pic">
                 <div class="wolf-info">
     
-                    <div class="wolf-details${index % 2 == 0 ? "-1" : "-2"}">
-                        <div class="wolf-name-age${index % 2 == 0 ? "-1" : "-2"}">
+                    <div class="wolf-details${index % 2 === 0 ? "-1" : "-2"}">
+                        <div class="wolf-name-age${index % 2 === 0 ? "-1" : "-2"}">
                             <h2 class="wolf-name">${lobinho.nome}</h2>
                             <p class="wolf-age">Idade: ${lobinho.idade} anos</p>
                         </div>
@@ -32,7 +31,7 @@ function atualizarLayout(lobinhos) {
                     </div>
     
                     <p
-                        class="wolf-text${index % 2 == 0 ? "-1" : "-2"}"
+                        class="wolf-text${index % 2 === 0 ? "-1" : "-2"}"
                         >${lobinho.descricao}</p>
                 </div>
             </div>
@@ -42,21 +41,47 @@ function atualizarLayout(lobinhos) {
 }
 
 
+function returnTextFromFilter() {
+    const name = document.getElementById("wolf-filter").value
+    return name.length > 0 ? name : ""
+}
+
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const lobinhos = await buscarLobinhos();
-        atualizarLayout(lobinhos);
+        const dados = await handleLobinhos();
+        atualizarLayout(dados);
     } catch (error) {
         console.error("Erro ao carregar:", error);
     }
 });
 
-const searchBar = document.querySelector("#wolf-filter");
-document.addEventListener("keydown", async (e) => {
-    if (e.key === "Enter") {
-        let nameFilter = searchBar.value;
-        const lobinhos = await carregarLobinhos(nameFilter);
-        atualizarLayout(lobinhos);
+let isFiltering = false
+const box = document.getElementById("adopt-checkbox")
+box.addEventListener("change", async () => {
+    try {
+    
+        const nomeLobo = returnTextFromFilter()
+        isFiltering = isFiltering ? false : true
+        const dados = await handleLobinhos({ nome : nomeLobo, adotado : box.checked });
+        atualizarLayout(dados);
+
+    } catch (error) {
+        console.error("Erro ao carregar:", error);
     }
 })
+
+document.addEventListener("keydown", async (e) => {
+    if (e.key === "Enter") {
+        try {
+            
+            const nomeLobo = returnTextFromFilter()
+            const dados = await handleLobinhos({ nome : nomeLobo, adotado : box.checked });
+            atualizarLayout(dados);
+
+        } catch (error) {
+            console.error("Erro ao carregar:", error);
+        }
+    }
+})
+
